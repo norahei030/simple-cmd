f32898acef86dc712e889936bdb2bf0586c62caf
Thai Do <thai.do@capgemini.com>
2023-08-08 12:09:16 +0200

    Implemented move command.

---
+++ src/main/java/cmd/commands/move/MoveCommand.java
@@ -0,0 +1,53 @@
+package cmd.commands.move;
+
+import cmd.SimpleCmd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * "Move" command class
+ * <p/>
+ * Executing the command moves a file or directory to a different location (and/or renames it)..
+ *
+ * @see SimpleCmd#getCurrentLocation()
+ * @see SimpleCmd#setCurrentLocation(File)
+ */
+@Command(name = "move", //
+         description = "Moves files and directories to different locations (and/or renames them)", //
+         mixinStandardHelpOptions = true)
+public class MoveCommand implements Runnable {
+    private static final Logger LOG = LoggerFactory.getLogger(MoveCommand.class);
+    @Parameters(index = "0", description = "path of the file to copy")
+    private Path source;
+    @Parameters(index = "1", description = "path to move file to")
+    private Path target;
+
+    @Override
+    public void run() {
+        File sourceFile = source.toFile();
+        File targetFile = target.toFile();
+        try {
+            final String sourceFileName = sourceFile.getName();
+            File targetFinalFile;
+            if (!target.endsWith(sourceFileName) && targetFile.isDirectory()) {
+                final String targetWithSourceFilename = target.toAbsolutePath().toString().concat(FileSystems.getDefault().getSeparator() + sourceFileName);
+                targetFinalFile = new File(targetWithSourceFilename);
+            } else {
+                targetFinalFile = targetFile;
+            }
+            Files.move(source, targetFinalFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            LOG.info("{} has been moved to {} \n", sourceFile, targetFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
