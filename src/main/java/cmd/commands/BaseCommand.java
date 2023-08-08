3048559754c3a6a48f2734f15068f4ea77e54cc5
stepgo <stephen.go@capgemini.com>
2023-08-08 10:50:36 +0200

    feature 1 - init

--- src/main/java/cmd/commands/BaseCommand.java
+++ src/main/java/cmd/commands/BaseCommand.java
@@ -1,5 +1,6 @@
 package cmd.commands;
 
+import cmd.commands.cd.CdCommand;
 import cmd.commands.copy.CopyCommand;
 import cmd.commands.del.DelCommand;
 import cmd.commands.dir.DirCommand;
@@ -18,7 +19,7 @@ import picocli.CommandLine.Command;
         name = "cmd",
         description = "base command",
         mixinStandardHelpOptions = true,
-        subcommands = {DirCommand.class, DelCommand.class, CopyCommand.class})
+        subcommands = {CdCommand.class, DirCommand.class, DelCommand.class, CopyCommand.class})
 public class BaseCommand implements Runnable {
     public BaseCommand() {
         /* intentionally empty */
