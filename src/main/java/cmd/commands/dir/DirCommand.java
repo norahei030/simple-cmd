515c72882f4c588b04980d565cb7de91a38ccb65
Gilein, Astrid <extern.astrid.gilein@volkswagen.de>
2023-08-09 09:54:47 +0200

    Feature-3: Expanded dir command

--- src/main/java/cmd/commands/dir/DirCommand.java
+++ src/main/java/cmd/commands/dir/DirCommand.java
@@ -3,9 +3,11 @@ package cmd.commands.dir;
 import cmd.SimpleCmd;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.stream.Stream;
 
@@ -26,6 +28,8 @@ import static picocli.CommandLine.Option;
 public class DirCommand implements Runnable {
 
     private static final Logger LOG = LoggerFactory.getLogger(DirCommand.class);
+    @CommandLine.Parameters(index = "0", description = "path to copy file to", arity = "0..1")
+    private File target;
     @Option(names = {"-f", "--files"}, description = "print only file names, directories will not be listed")
     private boolean filesOnly;
     @Option(names = {"-s", "--sort"}, description = "possible values are {asc, desc} for ascending / descending order")
@@ -38,7 +42,12 @@ public class DirCommand implements Runnable {
 
     @Override
     public void run() {
-        listFilesInDirectory(SimpleCmd.getCurrentLocation());
+        if(target!= null && target.isDirectory()) {
+            listFilesInDirectory(target);
+        }
+        else {
+            listFilesInDirectory(SimpleCmd.getCurrentLocation());
+        }
     }
 
     private void listFilesInDirectory(File directory) {
