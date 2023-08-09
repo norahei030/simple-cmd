1751234c93f7296a6e8d0637b83720f7214b28d7
Gilein, Astrid <extern.astrid.gilein@volkswagen.de>
2023-08-09 10:12:56 +0200

    Feature-7: Created date command. Prints out date and time.

--- src/main/java/cmd/commands/BaseCommand.java
+++ src/main/java/cmd/commands/BaseCommand.java
@@ -2,6 +2,7 @@ package cmd.commands;
 
 import cmd.commands.cd.CdCommand;
 import cmd.commands.copy.CopyCommand;
+import cmd.commands.date.DateCommand;
 import cmd.commands.del.DelCommand;
 import cmd.commands.dir.DirCommand;
 import cmd.commands.mkdir.MkdirCommand;
@@ -19,7 +20,7 @@ import picocli.CommandLine.Command;
  * @see <a href="https://picocli.info/">picocli Documentation</a>
  */
 @Command(name = "cmd", description = "base command", mixinStandardHelpOptions = true,
-         subcommands = {CdCommand.class, DirCommand.class, DelCommand.class, CopyCommand.class, MoveCommand.class, MkdirCommand.class, FindCommand.class})
+         subcommands = {CdCommand.class, DateCommand.class, DirCommand.class, DelCommand.class, CopyCommand.class, MoveCommand.class, MkdirCommand.class, FindCommand.class})
 public class BaseCommand implements Runnable {
     public BaseCommand() {
         /* intentionally empty */
---
+++ src/main/java/cmd/commands/date/DateCommand.java
@@ -0,0 +1,39 @@
+package cmd.commands.date;
+
+import cmd.SimpleCmd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * "Change Directory" command class
+ * <p/>
+ * Executing the command lists all the files and folders in the current working directory.
+ *
+ * @see SimpleCmd#getCurrentLocation() 
+ * @see SimpleCmd#setCurrentLocation(File) 
+ */
+@Command(
+        name = "date",
+        description = "Print current date",
+        mixinStandardHelpOptions = true)
+public class DateCommand implements Runnable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DateCommand.class);
+
+
+    public DateCommand() {
+        /* intentionally empty */
+    }
+
+    @Override
+    public void run() {
+        String timeStamp = new SimpleDateFormat("dd.MM.yyyy HH:mm").format(Calendar.getInstance().getTime());
+        LOG.info(timeStamp, "\n");
+    }
+}
